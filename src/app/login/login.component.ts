27b72a5ba3adfcd8268c0c859487fe7e49c706ff
falkune <iboudiokel@gmail.com>
2024-07-25 10:45:25 +0200

    envoie

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,12 +1,25 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
+
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+
+  loginForm: FormGroup;
+
+  constructor(private formBuilder: FormBuilder){
+    this.loginForm = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: [ '', [Validators.required, Validators.minLength(6)]]
+    })
+  }
+
 
 }
